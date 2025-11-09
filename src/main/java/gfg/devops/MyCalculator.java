3dcbe2f2dc478c48f8c2390a88d7278eec5d7462
vinay <vinayveerashekar@gmail.com>
2025-11-09 07:34:18 +0100

    My Calculator Project

---
+++ src/main/java/gfg/devops/MyCalculator.java
@@ -0,0 +1,27 @@
+package gfg.devops;
+
+public class MyCalculator {
+
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int diff(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public int mult(int a, int b)
+	{
+		return a*b;
+	}
+	public static void main(String[] args) {
+		
+		MyCalculator calc = new MyCalculator();
+		System.out.println("sum is:"+calc.sum(20, 10));
+		System.out.println("diff is:"+calc.diff(20, 10));
+		System.out.println("mult is:"+calc.mult(20, 10));
+	}
+
+}
